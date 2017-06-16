4eb11255c2f2a4d8acc8351cd4665875aa1012d2
Bryan Carlson <bryan.r.carlson@gmail.com>
2017-06-15 17:07:50 -0700

    Switched to using robocopy instead of manual

--- VmWithLoggerNetToBlob/copy-loggernet-data-to-blob.ps1
+++ VmWithLoggerNetToBlob/copy-loggernet-data-to-blob.ps1
@@ -5,9 +5,10 @@
 
 param([string]$dest, [string]$path, [string]$backup, [string]$logpartial)
 
-#$path = "C:\Files\LtarBoydNorth\"
-#$backup = "C:\Files\backups\LtarBoydNorth"
-#$dest = "https://ltarcafdatastream.blob.core.windows.net/boydnorth-ectower/raw"
+$path = "C:\Users\brcarlson\Desktop\EcTower\CookEast"
+$backup = "C:\Users\brcarlson\Desktop\EcTowerBackup\CookEast"
+$dest = "https://ltarcafdatastream.blob.core.windows.net/ectower-cookeast/raw"
+$logpartial = "C:\Users\brcarlson\Desktop\logs\cookeast"
 
 # Program expects a file containing the Azure Access Key to the blob storage account.  Put the key in quotes. 
 $key = Get-Content .\blob-key.private
@@ -40,24 +41,25 @@ if($itr -ge $numtries)
 
 "$([Environment]::NewLine)# Moving files to backup..." >> $log
 if($LASTEXITCODE -eq 0)
-    {
-    $files = Get-ChildItem -Path $path -Recurse
-    $files | Move-Item -Destination $backup -Verbose -Force *>> $log
+{
+    #$files = Get-ChildItem -Path $path -Recurse
+    #$files | Move-Item -Destination $backup -Verbose -Force *>> $log
+    robocopy.exe $path $backup /S /MOV >> $log
 
-    "$([Environment]::NewLine)# Checking success..." >> $log
-    foreach($file in $files)
-    {
-        $filename = [System.IO.Path]::GetFileName($file)
-        $oldpath = [System.IO.Path]::Combine($path,$filename)
-        $newpath = [System.IO.Path]::Combine($backup, $filename)
-
-        if(![System.IO.File]::Exists($oldpath) -And [System.IO.File]::Exists($newpath))
-        {
-            "Successfully moved: $oldpath to $newpath" >> $log
-        }
-        else
-        {
-            "Failed to move $oldpath to $newpath" >> $log
-        }
-    }
+    #"$([Environment]::NewLine)# Checking success..." >> $log
+    #foreach($file in $files)
+    #{
+    #    $filename = [System.IO.Path]::GetFileName($file)
+    #    $oldpath = [System.IO.Path]::Combine($path,$filename)
+    #    $newpath = [System.IO.Path]::Combine($backup, $filename)
+    #
+    #    if(![System.IO.File]::Exists($oldpath) -And [System.IO.File]::Exists($newpath))
+    #    {
+    #        "Successfully moved: $oldpath to $newpath" >> $log
+    #    }
+    #    else
+    #    {
+    #        "Failed to move $oldpath to $newpath" >> $log
+    #    }
+    #}
 }
